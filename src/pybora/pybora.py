84dfa656b0b882fcd06aa3b2ebbacf357444e456
Marco Bonici <bonici.marco@gmail.com>
2023-03-09 23:05:51 +0100

    Added a few things in the wrapper

--- src/pybora/pybora.py
+++ src/pybora/pybora.py
@@ -7,9 +7,9 @@ jl.seval("using SimpleChains")
 jl.seval("using BSON")
 jl.seval("using Static")
 
-bora_compute_Xil = jl.seval('Bora.get_ξℓ')
-bora_compute_Xils = jl.seval('Bora.get_ξℓs')
-load_emu_jl = jl.seval('BSON.load')
+__bora_compute_Xil = jl.seval('Bora.get_ξℓ')
+__bora_compute_Xils = jl.seval('Bora.get_ξℓs')
+__load_emu_jl = jl.seval('BSON.load')
 
 def compute_Xils(*args):
     my_list = [elem for elem in args]
@@ -19,15 +19,15 @@ def compute_Xils(*args):
     else:
          for i in range(len(args)-2):
             my_list[i] = jl.collect(my_list[i])
-    Pl = bora_compute_Xils(*my_list)
+    Pl = __bora_compute_Xils(*my_list)
     return np.array(Pl)
 
 def compute_Xil(cosmo, emu):
-    Xil = bora_compute_Xil(jl.collect(cosmo), emu)
+    Xil = __bora_compute_Xil(jl.collect(cosmo), emu)
     return np.array(Xil)
 
 def load_emu(path):
-    loaded = load_emu_jl(path)
+    loaded = __load_emu_jl(path)
     emu = loaded["ξℓs"]
     return emu
 
